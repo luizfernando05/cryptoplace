7b95bc1f97fec9a049c480877915dda80d696579
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2024-08-25 19:23:10 -0300

    :sparkles: feat: add structure to Home.jsx component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,23 @@ import React from "react";
 import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="hero">
+        <h1>
+          Largest <br /> Crypto Marketplace
+        </h1>
+        <p>
+          Welcome to the world's largest cryptocurrency marketplace. Sing up to
+          explore more about cryptos.
+        </p>
+        <form>
+          <input type="text" placeholder="Search crypto..." />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
