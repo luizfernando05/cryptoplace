a983a033fb994fafd40c7314097cb39e863e6752
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2024-08-25 15:14:45 -0300

    :sparkles: feat: add structure to Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,30 @@
 import React from "react";
 import "./Navbar.css";
+import logo from "../../assets/cryptoplace-logo.svg";
+import arrowUp from "../../assets/arrow-up-right.svg";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <img src={logo} alt="Logo from CryptoPlace" />
+      <ul>
+        <li>Home</li>
+        <li>Features</li>
+        <li>Pricing</li>
+        <li>Blog</li>
+      </ul>
+      <div className="nav-right">
+        <select>
+          <option value="usd">USD</option>
+          <option value="eur">EUR</option>
+          <option value="brl">BRL</option>
+        </select>
+        <button>
+          SignUp <img src={arrowUp} alt="" />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
