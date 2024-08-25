b7586dc2ac767a398f08dea35b6f3f62aa31e7a4
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2024-08-25 20:35:08 -0300

    :sparkles: feat: add currency selection handler to Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Navbar.css";
 import logo from "../../assets/cryptoplace-logo.svg";
 import arrowUp from "../../assets/arrow-up-right.svg";
+import { CoinContext } from "../../context/CoinContext";
 
 const Navbar = () => {
+  const { setCurrency } = useContext(CoinContext);
+
+  const currencyHandler = (event) => {
+    switch (event.target.value) {
+      case "usd": {
+        setCurrency({ name: "usd", Symbol: "$" });
+        break;
+      }
+      case "eur": {
+        setCurrency({ name: "eur", Symbol: "$" });
+        break;
+      }
+      case "brl": {
+        setCurrency({ name: "brl", Symbol: "R$" });
+        break;
+      }
+      default: {
+        setCurrency({ name: "usd", Symbol: "$" });
+        break;
+      }
+    }
+  };
+
   return (
     <div className="navbar">
       <img src={logo} alt="Logo from CryptoPlace" className="logo" />
@@ -14,7 +38,7 @@ const Navbar = () => {
         <li>Blog</li>
       </ul>
       <div className="nav-right">
-        <select>
+        <select onChange={currencyHandler}>
           <option value="usd">USD</option>
           <option value="eur">EUR</option>
           <option value="brl">BRL</option>
