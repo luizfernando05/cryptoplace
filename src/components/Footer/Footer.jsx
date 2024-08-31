12540af26004cddf39ae94612ac0f276f3b504da
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2024-08-31 14:35:15 -0300

    :lipstick: style: add styles to Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div>
+    <div className="footer">
       <p>Copyright @ 2024, Cryptoplace - All rights reserveds</p>
     </div>
   );
