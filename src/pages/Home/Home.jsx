ba55a42d06a0843530eebd2fc98681b3e5f277dc
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2024-08-31 14:05:27 -0300

    :sparkles: feat: add logic to display coin values in Home component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./Home.css";
+import { CoinContext } from "../../context/CoinContext";
 
 const Home = () => {
+  const { allCoin, currency } = useContext(CoinContext);
+  const [displayCoin, setDisplayCoin] = useState([]);
+
+  useEffect(() => {
+    setDisplayCoin(allCoin);
+  }, [allCoin]);
+
   return (
     <div className="home">
       <div className="hero">
@@ -26,6 +34,18 @@ const Home = () => {
           <p style={{ textAlign: "center" }}>24H Change</p>
           <p className="market-cap">Market Cap</p>
         </div>
+        {displayCoin.slice(0, 10).map((item, index) => (
+          <div className="table-layout" key={index}>
+            <p>{item.market_cap_rank}</p>
+            <div>
+              <img src={item.image} alt="" />
+              <p>{item.name + " - " + item.symbol}</p>
+            </div>
+            <p>
+              {currency.symbol} {item.current_price}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
