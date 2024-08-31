a6c291e1bf0f655eab359945f117e39591b3c0b2
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2024-08-31 14:31:18 -0300

    :sparkles: feat: add initial structure to Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <p>Copyright @ 2024, Cryptoplace - All rights reserveds</p>
+    </div>
+  );
 };
 
 export default Footer;
